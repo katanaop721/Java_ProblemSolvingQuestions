7bde387470bf058a1e287e8ffa09e5a1f6ea9399
Anku Kumar <katanaop6@gmail.com>
2024-12-26 01:21:45 +0530

    .

---
+++ EasyLevelDSA/Array_Questions/CountSubarraysumEqualsK.java
@@ -0,0 +1,21 @@
+package EasyLevelDSA.Array_Questions;
+
+public class CountSubarraysumEqualsK {
+    public static void main(String[] args) {
+        int[] arr={1,2,3};
+        int target=3;
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i;j<arr.length;j++){
+                int temp=0;
+                for(int k=i;k<=j;k++){
+                    temp+=arr[k];
+                }
+                if(temp==target){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
